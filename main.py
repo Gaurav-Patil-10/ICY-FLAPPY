8422fcc7e67a3bcb4a582923b2fdf487fe463491
Gaurav Patil <gauravptl023@gmail.com>
2020-08-05 01:54:12 +0530

    directory changes

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH , SCREEN_HEIGHT))
 GROUNDY = SCREEN_HEIGHT
 GAME_SPRITES = {}
 GAME_SOUNDS = {}
-PLAYER = "/ICY FLAPPY/Sprites/bird.png"
-BACKGROUND = "/ICY FLAPPY/Sprites/background.png"
-OBSTACLES = '/ICY FLAPPY/Sprites/obstacles.png'
+PLAYER = "Sprites/bird.png"
+BACKGROUND = "Sprites/background.png"
+OBSTACLES = 'Sprites/obstacles.png'
+
+
+# Defining the Functions for the game 
+
+
+
+
+
+
 
 
 # driver code from where the game will start
@@ -24,21 +33,21 @@ if __name__ == "__main__":
     FPS_CLOCK = pygame.time.Clock()
     pygame.display.set_caption("ICY FLAPPY")
     GAME_SPRITES['numbers'] = (
-        pygame.image.load('/ICY FLAPPY/Sprites/0.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/1.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/2.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/3.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/4.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/5.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/6.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/7.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/8.png').convert_alpha(),
-        pygame.image.load('/ICY FLAPPY/Sprites/9.png').convert_alpha()
+        pygame.image.load('Sprites\\0.png').convert_alpha(),
+        pygame.image.load('Sprites\\1.png').convert_alpha(),
+        pygame.image.load('Sprites\\2.png').convert_alpha(),
+        pygame.image.load('Sprites\\3.png').convert_alpha(),
+        pygame.image.load('Sprites\\4.png').convert_alpha(),
+        pygame.image.load('Sprites\\5.png').convert_alpha(),
+        pygame.image.load('Sprites\\6.png').convert_alpha(),
+        pygame.image.load('Sprites\\7.png').convert_alpha(),
+        pygame.image.load('Sprites\\8.png').convert_alpha(),
+        pygame.image.load('Sprites\\9.png').convert_alpha()
 
     )
 
-    GAME_SPRITES['message'] = pygame.image.load('/ICY FLAPPY/Sprites/message.png').convert_alpha()
-    GAME_SPRITES['base'] = pygame.image.load('/ICY FLAPPY/Sprites/base.png').convert_alpha()
+    GAME_SPRITES['message'] = pygame.image.load('Sprites/message.png').convert_alpha()
+    GAME_SPRITES['base'] = pygame.image.load('Sprites/base.png').convert_alpha()
     GAME_SPRITES['obstacles'] = (
         pygame.image.load(OBSTACLES).convert_alpha(),
         pygame.transform.rotate(pygame.image.load(OBSTACLES).convert_alpha() , 180)
@@ -46,23 +55,23 @@ if __name__ == "__main__":
 
     # Game Sounds 
 
-    GAME_SOUNDS['die'] = pygame.mixer.Sound('/ICY FLAPPY/audio/die.wav')
-    GAME_SOUNDS['hit'] = pygame.mixer.Sound('/ICY FLAPPY/audio/hit.wav')
-    GAME_SOUNDS['point'] = pygame.mixer.Sound('/ICY FLAPPY/audio/point.wav')
-    GAME_SOUNDS['swoosh'] = pygame.mixer.Sound('/ICY FLAPPY/audio/swoosh.wav')
-    GAME_SOUNDS['wing'] = pygame.mixer.Sound('/ICY FLAPPY/audio/wing.wav')
+    GAME_SOUNDS['die'] = pygame.mixer.Sound('audio/die.wav')
+    GAME_SOUNDS['hit'] = pygame.mixer.Sound('audio/hit.wav')
+    GAME_SOUNDS['point'] = pygame.mixer.Sound('audio/point.wav')
+    GAME_SOUNDS['swoosh'] = pygame.mixer.Sound('audio/swoosh.wav')
+    GAME_SOUNDS['wing'] = pygame.mixer.Sound('audio/wing.wav')
 
     # background , Player Sprites
 
-    GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert_aplha()
-    GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_aplha()
+    GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert_alpha()
+    GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
 
     # game Loop
 
     while True:
 
-        welcome_screen()
-        main_game()
+        welcome_screen()   # Shows the welcome screen until user click the play OR tap 
+        main_game()        # this is the main Game Function
 
 
 
